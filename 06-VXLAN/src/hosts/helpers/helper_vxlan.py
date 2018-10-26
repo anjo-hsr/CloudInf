7af91b3900f674b69d8d2fcbc96ebe067dceb479
AndiHSR <andi.hoerler@hsr.ch>
2018-10-26 03:15:52 +0200

    Replace hash function with sha256 to generate unique VXLAN id

--- 06-VXLAN/src/hosts/helpers/helper_vxlan.py
+++ 06-VXLAN/src/hosts/helpers/helper_vxlan.py
@@ -1,3 +1,5 @@
+from hashlib import sha256
+
 from helper_vlan import generate_vlan_array
 from helper_switch import get_switch_ip_address
 
@@ -6,13 +8,18 @@ def generate_vxlan_tunnels(ip_address, max_vlans, switch):
     ips = [ip_address, get_switch_ip_address(switch)]
     for vlan in generate_vlan_array(max_vlans):
         print(str(min(ips)) + "-" + str(max(ips)) + " - vlan" + str(vlan))
-        vxlan_tag = 100 + hash(str(min(ips)) + "-" + str(max(ips)) +
-                         "-" + str(vlan)) & 10000000
+        vxlan_tag = __connection_string_to_hash(ips, vlan)
 
         switch.cmdPrint(
             "ovs-vsctl add-port " + str(switch) + " vx" + str(vxlan_tag) + " tag=" + str(vlan) +
             " -- set interface vx" + str(vxlan_tag) + " type=vxlan option:remote_ip=" + ip_address +
             " option:key=" + str(vxlan_tag))
+
+
+def __connection_string_to_hash(ips, vlan):
+    ip_string = str(min(ips)) + "-" + str(max(ips)) + "-" + str(vlan)
+    hash_value = sha256(ip_string.encode("utf-8"))
+    return int(hash_value.hexdigest()[:4], 16) + 4096
 
 
 def map_vxlans(network, peers, max_vlans):
