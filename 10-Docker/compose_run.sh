ed40e239855cb6946a2857bae392b5787a2230e5
AndiHSR <andi.hoerler@hsr.ch>
2018-11-21 19:00:30 +0100

    Delete self created networks

--- 10-Docker/compose_run.sh
+++ 10-Docker/compose_run.sh
@@ -7,7 +7,9 @@ echo -e "\nContainers stopped\n-----------\nStart deleting containers\n"
 docker rm $(docker ps -aq)
 echo -e "\nContainers deleted\n-----------\nStart deleting all cloud-inf images\n"
 docker rmi cloud-inf_web cloud-inf_api cloud-inf_db
-echo -e "\nImages deleted\n-----------"
+echo -e "\nImages deleted\n-----------\nStart deleting all cloud-inf networks\n"
+docker network rm cloud-inf_intNetwork cloud-inf_dbNetwork
+echo -e "\nNetworks deleted\n-----------"
 
 echo -e "Building with docker-compose"
 docker-compose up
