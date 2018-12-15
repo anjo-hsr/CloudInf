956c6705b0135aacdb975a09b8d3f30fa7e69871
AndiHSR <andi.hoerler@hsr.ch>
2018-12-15 11:40:02 +0100

    Delete unused helper

--- 14-PYang/src/helpers/xml_parser.py
+++
@@ -1,7 +0,0 @@
-import xmltodict
-
-
-def add_filter(config, filter):
-    config_dict = xmltodict.parse(config.xml)
-    config_dict["rpc-reply"]["data"] = xmltodict.parse(filter)
-    return xmltodict.unparse(config_dict)
