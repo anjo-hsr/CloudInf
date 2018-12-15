87c1baa1c89658a9a955dff88851cc10674be291
AndiHSR <andi.hoerler@hsr.ch>
2018-12-15 15:35:20 +0100

    Refactor to new function

--- 14-PYang/src/helpers/configs/edit_config.py
+++ 14-PYang/src/helpers/configs/edit_config.py
@@ -19,21 +19,7 @@ def add_xml_config(m, socket_string):
         print("\nYou have added a new bgp neighborship to this router. Now you " +
               get_info_string("could switch to another router") + " to create the neighborship there as well.")
         print_connection(socket_string)
-        add_now = input("Would you like to do that now. [Y/n]")
-
-        if add_now != "Y":
-            return
-
-        print(get_info_string("OK, please enter the new connection parameters to continue:"))
-        connection_parameters = get_connection()
-        m2 = generate_connection(connection_parameters)
-
-        print(get_info_string("Now enter the parameters to delete the remote bgp connection:"))
-        add_config = get_bgp_neighbor_add_config()
-
-        alter_config(m2, add_config.xml)
-        print(get_info_string("Closing connection from external router"))
-        m2.close_session()
+        alter_second_router("add")
 
 
 def delete_xml_config(m, socket_string):
@@ -48,22 +34,28 @@ def delete_xml_config(m, socket_string):
     if delete_config.key == "bgp":
         print("\nYou have deleted the bgp neighborship from this router. Now you " +
               get_info_string("could switch to another router") + " to delete the neighborship there as well.")
-        print_connection(socket_string)
-        delete_now = input("Would you like to do that now. [Y/n]")
+        alter_second_router("delete")
 
-        if delete_now != "Y":
-            return
 
-        print(get_info_string("OK, please enter the new connection parameters to continue:"))
-        connection_parameters = get_connection()
-        m2 = generate_connection(connection_parameters)
+def alter_second_router(alter_type):
+    alter_now = input("Would you like to do that now. [Y/n]")
+    if alter_now != "Y":
+        return
 
-        print(get_info_string("Now enter the parameters to delete the remote bgp connection:"))
-        delete_config = get_bgp_neighbor_delete_config()
+    print(get_info_string("OK, please enter the new connection parameters to continue:"))
+    connection_parameters = get_connection()
+    m2 = generate_connection(connection_parameters)
 
-        alter_config(m2, delete_config.xml)
-        print(get_info_string("Closing connection from external router"))
-        m2.close_session()
+    print(get_info_string("Now enter the parameters to " + alter_type + " the remote bgp connection:"))
+
+    if alter_type is "add":
+        alter_config_map = get_bgp_neighbor_add_config()
+    else:
+        alter_config_map = get_bgp_neighbor_delete_config()
+
+    alter_config(m2, alter_config_map.xml)
+    print(get_info_string("Closing connection from external router"))
+    m2.close_session()
 
 
 def alter_config(m, alter_config_xml):
